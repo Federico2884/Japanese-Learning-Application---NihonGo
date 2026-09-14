b832a5d2119de3516302fca83dbb83cabbda53ef
Federico2884 <ricoam881@gmail.com>
2026-09-14 11:07:58 +0800

    feat(practice): add writing practice page
    - halaman /tulis/:script/:char dengan tiga tingkat: jiplak, dengan petunjuk, dari ingatan
    - kartu huruf, pelafalan, demo urutan goresan, dan navigasi antarhuruf
    - kartu hasil dengan akurasi, jumlah kesalahan, dan tombol huruf berikutnya
    - tata letak dua kolom untuk tablet mendatar/laptop, satu kolom untuk HP
    - beranda menautkan latihan hiragana dan katakana

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import WritePractice from './pages/WritePractice'
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/tulis" element={<Navigate to="/tulis/hiragana/あ" replace />} />
+      <Route path="/tulis/:script" element={<WritePractice />} />
+      <Route path="/tulis/:script/:char" element={<WritePractice />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   )
 }
